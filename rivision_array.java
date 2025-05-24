5a342763997fcc1204e9d1d0a59f53927261d3d8
DeveloperManikDas <developermanikdas@gmail.com>
2025-05-24 22:19:31 +0530

    tapping rainwater problem

--- rivision_array.java
+++ rivision_array.java
@@ -105,6 +105,30 @@ class rivisionArray {
         return maxSum;
     }
 
+
+    public int trap(int[] height) {
+    int left = 0, right = height.length - 1;
+    int leftMax = 0, rightMax = 0;
+    int water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax)
+                leftMax = height[left];
+            else
+                water += leftMax - height[left];
+            left++;
+        } else {
+            if (height[right] >= rightMax)
+                rightMax = height[right];
+            else
+                water += rightMax - height[right];
+            right--;
+        }
+    }
+    return water;
+}
+
     public static void main(String[] args) {
         int arr[] = { 1, -13, 5, 7 };
 
