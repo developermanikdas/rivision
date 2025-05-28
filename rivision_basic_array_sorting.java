76cbc2b60551d2d7a8ba0773a4f9b9bd6dec4ed5
DeveloperManikDas <developermanikdas@gmail.com>
2025-05-28 22:55:53 +0530

    bubble sort practice

--- rivision_basic_array_sorting.java
+++ rivision_basic_array_sorting.java
@@ -1,6 +1,34 @@
 public class rivision_basic_array_sorting {
 
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        // Outer loop for each pass
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element is greater than the next
+                if (arr[j] > arr[j + 1]) {
+                    // Swapping elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true; // A swap happened
+                }
+            }
+
+            // Optimization: if no two elements were swapped in the inner loop, break early
+            if (!swapped)
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 1, 4, 2 };
+        bubbleSort(arr);
     }
 }
