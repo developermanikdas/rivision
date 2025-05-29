608854cfb155c1e18ee9d7024ecaad5cb37f4f67
DeveloperManikDas <developermanikdas@gmail.com>
2025-05-29 20:38:23 +0530

    practice

--- rivision_basic_array_sorting.java
+++ rivision_basic_array_sorting.java
@@ -27,6 +27,42 @@ public class rivision_basic_array_sorting {
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];  // Current element to insert
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];  // Shift element to the right
+                j--;
+            }
+            arr[j + 1] = key;  // Place key at correct position
+        }
+    }
+
+    // Utility function to print array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the sort
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        System.out.println("Before sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("After sorting:");
+        printArray(arr);
+    }
     public static void main(String[] args) {
         int arr[] = { 3, 1, 4, 2 };
         bubbleSort(arr);
