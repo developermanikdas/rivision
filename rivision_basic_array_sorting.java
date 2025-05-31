054c6f20c03aa3a28c94067a32fc4e9c1d2753c1
DeveloperManikDas <developermanikdas@gmail.com>
2025-05-31 22:28:10 +0530

    practice

--- rivision_basic_array_sorting.java
+++ rivision_basic_array_sorting.java
@@ -51,7 +51,58 @@ public class rivision_basic_array_sorting {
         System.out.println();
     }
 
- 
+    // Main function to sort an array using merge sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int mid = left + (right - left) / 2;
+
+            // Recursively sort first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Function to merge two subarrays
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // Sizes of two subarrays
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        // Merge temp arrays
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        // Copy remaining elements of L[]
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        // Copy remaining elements of R[]
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
     public static void main(String[] args) {
         int arr[] = { 3, 1, 4, 2 };
         bubbleSort(arr);
