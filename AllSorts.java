cfe7fbff42da8b6292423713fdcb304980df4559
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-05 21:47:43 +0530

    practice

--- AllSorts.java
+++ AllSorts.java
@@ -191,7 +191,27 @@ public class AllSorts {
             }
         }
     }
+public class MatrixIO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
+        int[][] matrix = new int[rows][cols];
+
+        // Input
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i][j] = sc.nextInt();
+
+        // Output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
     // ✨ Main method to test
     public static void main(String[] args) {
         int[] arr = { 5, 2, 9, 1, 5, 6 };
