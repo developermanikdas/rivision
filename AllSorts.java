14c7e99f62cba706c7c5d87f966a330b80dd1000
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-07 22:32:18 +0530

    practice

--- AllSorts.java
+++ AllSorts.java
@@ -1,252 +1,48 @@
-import java.util.*;
-
 public class AllSorts {
 
-    // 1. Bubble Sort
-    public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < arr.length - i - 1; j++) {
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
                 }
             }
-            if (!swapped)
-                break;
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+
         }
     }
 
-    // 2. Selection Sort
-    public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[minIdx])
-                    minIdx = j;
-            int temp = arr[minIdx];
-            arr[minIdx] = arr[i];
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minPos = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
             arr[i] = temp;
         }
-    }
 
-    // 3. Insertion Sort
-    public static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i], j = i - 1;
-            while (j >= 0 && arr[j] > key)
-                arr[j + 1] = arr[j--];
-            arr[j + 1] = key;
+        for (int i : arr) {
+            System.out.println(i);
+
         }
     }
 
-    // 4. Merge Sort
-    public static void mergeSort(int[] arr, int l, int r) {
-        if (l < r) {
-            int m = (l + r) / 2;
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-            merge(arr, l, m, r);
-        }
-    }
-
-    private static void merge(int[] arr, int l, int m, int r) {
-        int[] left = Arrays.copyOfRange(arr, l, m + 1);
-        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
-        int i = 0, j = 0, k = l;
-        while (i < left.length && j < right.length)
-            arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
-        while (i < left.length)
-            arr[k++] = left[i++];
-        while (j < right.length)
-            arr[k++] = right[j++];
-    }
-
-    // 5. Quick Sort
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int p = partition(arr, low, high);
-            quickSort(arr, low, p - 1);
-            quickSort(arr, p + 1, high);
-        }
-    }
-
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high], i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return i + 1;
-    }
-
-    // 6. Heap Sort
-    public static void heapSort(int[] arr) {
-        int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
-        for (int i = n - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-            heapify(arr, i, 0);
-        }
-    }
-
-    private static void heapify(int[] arr, int n, int i) {
-        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-        if (largest != i) {
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-            heapify(arr, n, largest);
-        }
-    }
-
-    // 7. Counting Sort (Only for non-negative integers)
-    public static void countingSort(int[] arr) {
-        int max = Arrays.stream(arr).max().orElse(0);
-        int[] count = new int[max + 1];
-        for (int num : arr)
-            count[num]++;
-        int idx = 0;
-        for (int i = 0; i < count.length; i++)
-            while (count[i]-- > 0)
-                arr[idx++] = i;
-    }
-
-    // 8. Radix Sort
-    public static void radixSort(int[] arr) {
-        int max = Arrays.stream(arr).max().orElse(0);
-        for (int exp = 1; max / exp > 0; exp *= 10)
-            countingSortByDigit(arr, exp);
-    }
-
-    private static void countingSortByDigit(int[] arr, int exp) {
-        int[] output = new int[arr.length];
-        int[] count = new int[10];
-        for (int num : arr)
-            count[(num / exp) % 10]++;
-        for (int i = 1; i < 10; i++)
-            count[i] += count[i - 1];
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int digit = (arr[i] / exp) % 10;
-            output[--count[digit]] = arr[i];
-        }
-        System.arraycopy(output, 0, arr, 0, arr.length);
-    }
-
-    // 9. Bucket Sort (for [0, 1) float values)
-    public static void bucketSort(float[] arr) {
-        int n = arr.length;
-        if (n <= 0)
-            return;
-        List<Float>[] buckets = new List[n];
-        for (int i = 0; i < n; i++)
-            buckets[i] = new ArrayList<>();
-        for (float num : arr) {
-            int idx = (int) (n * num);
-            buckets[idx].add(num);
-        }
-        for (List<Float> bucket : buckets)
-            Collections.sort(bucket);
-        int index = 0;
-        for (List<Float> bucket : buckets)
-            for (float num : bucket)
-                arr[index++] = num;
-    }
-
-    public class TwoDArrayInput {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int[][] arr = new int[3][3];
-
-            // Input
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-
-            // Output
-            System.out.println("2D Array:");
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(arr[i][j] + " ");
-                }
-                System.out.println();
-            }
-        }
-    }
-public class MatrixIO {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-
-        int[][] matrix = new int[rows][cols];
-
-        // Input
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                matrix[i][j] = sc.nextInt();
-
-        // Output
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++)
-                System.out.print(matrix[i][j] + " ");
-            System.out.println();
-        }
-    }
-}
-
-
- int[][] matrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            // top row
-            for (int i = left; i <= right; i++)
-                System.out.print(matrix[top][i] + " ");
-            top++;
-
-            // right column
-            for (int i = top; i <= bottom; i++)
-                System.out.print(matrix[i][right] + " ");
-            right--;
-
-            // bottom row
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--)
-                    System.out.print(matrix[bottom][i] + " ");
-                bottom--;
-            }
-
-            // left column
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--)
-                    System.out.print(matrix[i][left] + " ");
-                left++;
-            }
-        }
-    // ✨ Main method to test
-    public static void main(String[] args) {
-        int[] arr = { 5, 2, 9, 1, 5, 6 };
-        quickSort(arr, 0, arr.length - 1); // change to test other sorts
-        System.out.println("Sorted: " + Arrays.toString(arr));
+        int[] arr = { 3, 1, 5, 4, 2, 0, 6, 7,-9 };
+        // bubbleSort(arr);
+        selectionSort(arr);
     }
 }
