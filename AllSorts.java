708554dd61781d8a6bdd6d4b65f1a18fe83e489d
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-08 22:36:23 +0530

    done

--- AllSorts.java
+++ AllSorts.java
@@ -39,6 +39,31 @@ public class AllSorts {
 
         }
     }
+ public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements of arr[0..i-1] that are greater than key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            // Place the key at its correct sorted position
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
 
     public static void main(String[] args) {
         int[] arr = { 3, 1, 5, 4, 2, 0, 6, 7,-9 };
