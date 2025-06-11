02143b711cd0b6add5497c2f97c20abba11beacd
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-11 22:02:36 +0530

    done

--- AllSorts.java
+++ AllSorts.java
@@ -39,7 +39,8 @@ public class AllSorts {
 
         }
     }
- public static void insertionSort(int[] arr) {
+
+    public static void insertionSort(int[] arr) {
         int n = arr.length;
 
         for (int i = 1; i < n; i++) {
@@ -64,8 +65,33 @@ public class AllSorts {
         System.out.println();
     }
 
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 3, 1, 5, 4, 2, 0, 6, 7,-9 };
+        int[] arr = { 3, 1, 5, 4, 2, 0, 6, 7, -9 };
         // bubbleSort(arr);
         selectionSort(arr);
     }
