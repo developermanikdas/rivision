ab5ca1c1b944c152ee8a1c131e955105b7e767ea
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-20 22:31:19 +0530

    done

--- MaxSubarraySumWithOneDeletion.java
+++ MaxSubarraySumWithOneDeletion.java
@@ -1,25 +1,25 @@
-import java.util.*;
-
-public class LongestIncreasingSubsequence {
-    public static int lengthOfLIS(int[] nums) {
-        List<Integer> tails = new ArrayList<>();
+public class MaxCircularSubarray {
+    public static int maxSubarraySumCircular(int[] nums) {
+        int total = 0;
+        int maxSum = nums[0], curMax = 0;
+        int minSum = nums[0], curMin = 0;
 
         for (int num : nums) {
-            int idx = Collections.binarySearch(tails, num);
-            if (idx < 0) idx = -(idx + 1); // insertion point
+            curMax = Math.max(curMax + num, num);
+            maxSum = Math.max(maxSum, curMax);
 
-            if (idx == tails.size()) {
-                tails.add(num);
-            } else {
-                tails.set(idx, num); // replace
-            }
+            curMin = Math.min(curMin + num, num);
+            minSum = Math.min(minSum, curMin);
+
+            total += num;
         }
 
-        return tails.size();
+        // If all numbers are negative, maxWrap would be 0 → wrong
+        return (maxSum > 0) ? Math.max(maxSum, total - minSum) : maxSum;
     }
 
     public static void main(String[] args) {
-        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
-        System.out.println("LIS Length: " + lengthOfLIS(nums));  // Output: 4
+        int[] nums = {5, -3, 5};
+        System.out.println("Max Circular Subarray Sum: " + maxSubarraySumCircular(nums));  // Output: 10
     }
 }
