7bd72ab6f7a320a17e6fede2e58c988e3f0bae26
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-21 20:14:38 +0530

    done

--- MaxSubarraySumWithOneDeletion.java
+++ MaxSubarraySumWithOneDeletion.java
@@ -1,25 +1,44 @@
-public class MaxCircularSubarray {
-    public static int maxSubarraySumCircular(int[] nums) {
-        int total = 0;
-        int maxSum = nums[0], curMax = 0;
-        int minSum = nums[0], curMin = 0;
-
+public class SplitArrayLargestSum {
+    public static int splitArray(int[] nums, int k) {
+        int left = 0, right = 0;
         for (int num : nums) {
-            curMax = Math.max(curMax + num, num);
-            maxSum = Math.max(maxSum, curMax);
-
-            curMin = Math.min(curMin + num, num);
-            minSum = Math.min(minSum, curMin);
-
-            total += num;
+            left = Math.max(left, num);
+            right += num;
         }
 
-        // If all numbers are negative, maxWrap would be 0 → wrong
-        return (maxSum > 0) ? Math.max(maxSum, total - minSum) : maxSum;
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static boolean canSplit(int[] nums, int k, int maxSum) {
+        int count = 1;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                count++;
+                currentSum = num;
+                if (count > k) return false;
+            } else {
+                currentSum += num;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        int[] nums = {5, -3, 5};
-        System.out.println("Max Circular Subarray Sum: " + maxSubarraySumCircular(nums));  // Output: 10
+        int[] nums = {7, 2, 5, 10, 8};
+        int k = 2;
+        System.out.println("Minimized Largest Subarray Sum: " + splitArray(nums, k));  // Output: 18
     }
 }
