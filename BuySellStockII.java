7e4a641f2f58b87d54b008ec68389dae81e0c646
DeveloperManikDas <developermanikdas@gmail.com>
2025-06-30 22:13:25 +0530

    done

--- BuySellStockII.java
+++ BuySellStockII.java
@@ -12,8 +12,43 @@ public class BuySellStockII {
         return profit;
     }
 
-    public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4};
-        System.out.println("Maximum Profit: " + maxProfit(prices));
+    public class BuySellStockIII {
+        public static int maxProfit(int[] prices) {
+            if (prices.length == 0)
+                return 0;
+
+            int n = prices.length;
+            int[] left = new int[n]; // Max profit if sold up to day i
+            int[] right = new int[n]; // Max profit if bought from day i
+
+            // Left pass
+            int minLeft = prices[0];
+            for (int i = 1; i < n; i++) {
+                minLeft = Math.min(minLeft, prices[i]);
+                left[i] = Math.max(left[i - 1], prices[i] - minLeft);
+            }
+
+            // Right pass
+            int maxRight = prices[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                maxRight = Math.max(maxRight, prices[i]);
+                right[i] = Math.max(right[i + 1], maxRight - prices[i]);
+            }
+
+            // Combine
+            int maxProfit = 0;
+            for (int i = 0; i < n; i++) {
+                maxProfit = Math.max(maxProfit, left[i] + right[i]);
+            }
+
+            return maxProfit;
+        }
+
+        public static void main(String[] args) {
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            System.out.println("Maximum Profit: " + maxProfit(prices));
+        }
+
     }
+
 }
