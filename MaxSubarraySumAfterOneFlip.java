e662790af246e1181143b0b5bdbe49784bed05a5
DeveloperManikDas <developermanikdas@gmail.com>
2025-07-02 22:23:22 +0530

    MaxSubarraySumAfterOneFlip

---
+++ MaxSubarraySumAfterOneFlip.java
@@ -0,0 +1,26 @@
+public class MaxSubarraySumAfterOneFlip {
+    public static int maxSubarraySumWithOneFlip(int[] arr) {
+        int n = arr.length;
+
+        int maxEndHere = arr[0];
+        int maxSoFar = arr[0];
+
+        int flipMaxEndHere = -arr[0]; // start by flipping first
+        int flipMaxSoFar = flipMaxEndHere;
+
+        for (int i = 1; i < n; i++) {
+            maxEndHere = Math.max(arr[i], maxEndHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndHere);
+
+            flipMaxEndHere = Math.max(-arr[i], flipMaxEndHere + arr[i]);
+            flipMaxSoFar = Math.max(flipMaxSoFar, flipMaxEndHere);
+        }
+
+        return Math.max(maxSoFar, flipMaxSoFar);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, -2, 0, 3 };
+        System.out.println("Maximum sum after one flip: " + maxSubarraySumWithOneFlip(arr));
+    }
+}
