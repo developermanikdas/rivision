20639c200ce6626c406e5a41148543d7bfa01554
DeveloperManikDas <developermanikdas@gmail.com>
2025-07-03 22:45:08 +0530

    done

--- MaxSubarraySumAfterOneFlip.java
+++ MaxSubarraySumAfterOneFlip.java
@@ -38,6 +38,25 @@ public class MaxSubarraySumAfterOneFlip {
         return maxLen;
     }
 
+
+    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+    if (k <= 1) return 0;
+
+    int prod = 1, left = 0, count = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+        prod *= nums[right];
+
+        while (prod >= k) {
+            prod /= nums[left++];
+        }
+
+        count += right - left + 1;
+    }
+
+    return count;
+}
+
     public static void main(String[] args) {
         int[] arr = { 1, -2, 0, 3 };
         System.out.println("Maximum sum after one flip: " + maxSubarraySumWithOneFlip(arr));
