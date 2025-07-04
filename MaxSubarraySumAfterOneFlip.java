9225371c2c0d9033e453a70d28596942bf9e3778
DeveloperManikDas <developermanikdas@gmail.com>
2025-07-04 21:39:59 +0530

    done

--- MaxSubarraySumAfterOneFlip.java
+++ MaxSubarraySumAfterOneFlip.java
@@ -1,3 +1,4 @@
+ import java.util.*;
 public class MaxSubarraySumAfterOneFlip {
     public static int maxSubarraySumWithOneFlip(int[] arr) {
         int n = arr.length;
@@ -38,24 +39,79 @@ public class MaxSubarraySumAfterOneFlip {
         return maxLen;
     }
 
-
     public static int numSubarrayProductLessThanK(int[] nums, int k) {
-    if (k <= 1) return 0;
+        if (k <= 1)
+            return 0;
 
-    int prod = 1, left = 0, count = 0;
+        int prod = 1, left = 0, count = 0;
 
-    for (int right = 0; right < nums.length; right++) {
-        prod *= nums[right];
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
 
-        while (prod >= k) {
-            prod /= nums[left++];
+            while (prod >= k) {
+                prod /= nums[left++];
+            }
+
+            count += right - left + 1;
         }
 
-        count += right - left + 1;
+        return count;
     }
 
-    return count;
+   
+
+public static int shortestSubarray(int[] nums, int k) {
+    int n = nums.length;
+    long[] prefix = new long[n + 1];
+    for (int i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
+    }
+
+    int minLen = n + 1;
+    Deque<Integer> deque = new ArrayDeque<>();
+
+    for (int i = 0; i <= n; i++) {
+        while (!deque.isEmpty() && prefix[i] - prefix[deque.peekFirst()] >= k) {
+            minLen = Math.min(minLen, i - deque.pollFirst());
+        }
+
+        while (!deque.isEmpty() && prefix[i] <= prefix[deque.peekLast()]) {
+            deque.pollLast();
+        }
+
+        deque.offerLast(i);
+    }
+
+    return minLen <= n ? minLen : -1;
 }
+
+   
+
+    public static int findMaxValueOfEquation(int[][] points, int k) {
+        int max = Integer.MIN_VALUE;
+        Deque<int[]> deque = new ArrayDeque<>();
+
+        for (int[] point : points) {
+            int x = point[0], y = point[1];
+
+            while (!deque.isEmpty() && x - deque.peekFirst()[1] > k) {
+                deque.pollFirst();
+            }
+
+            if (!deque.isEmpty()) {
+                max = Math.max(max, x + y + deque.peekFirst()[0]);
+            }
+
+            // Keep deque in decreasing order of (y - x)
+            while (!deque.isEmpty() && (y - x) >= deque.peekLast()[0]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(new int[] { y - x, x });
+        }
+
+        return max;
+    }
 
     public static void main(String[] args) {
         int[] arr = { 1, -2, 0, 3 };
