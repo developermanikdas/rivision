4796962ea2656d65139fde3029c4d200fd4658c2
DeveloperManikDas <developermanikdas@gmail.com>
2025-07-17 22:06:28 +0530

    SquaresOfASortedArray

--- maxSubArray.java
+++ maxSubArray.java
@@ -1,6 +1,6 @@
 public class maxSubArray {
     // Function to find the maximum subarray sum using Kadane's algorithm                                                                                                                       
-    public static int maxSubArray(int[] nums) {
+    public static int findMaxSubArray(int[] nums) {
         int maxSoFar = nums[0];
         int currentMax = nums[0];
 
@@ -15,15 +15,15 @@ public class maxSubArray {
     public static void main(String[] args) {
         // Test Case 1
         int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println("Maximum Subarray Sum (Example 1): " + maxSubArray(nums1));
+        System.out.println("Maximum Subarray Sum (Example 1): " + findMaxSubArray(nums1));
 
         // Test Case 2
         int[] nums2 = {1};
-        System.out.println("Maximum Subarray Sum (Example 2): " + maxSubArray(nums2));
+        System.out.println("Maximum Subarray Sum (Example 2): " + findMaxSubArray(nums2));
 
         // Test Case 3
         int[] nums3 = {5, 4, -1, 7, 8};
-        System.out.println("Maximum Subarray Sum (Example 3): " + maxSubArray(nums3));
+        System.out.println("Maximum Subarray Sum (Example 3): " + findMaxSubArray(nums3));
     }
 
 }
