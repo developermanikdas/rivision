445bb0b9dbe8f6544ab5b3ca89ac067c1c01b980
DeveloperManikDas <developermanikdas@gmail.com>
2025-07-29 22:49:07 +0530

    singleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,17 @@
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR operation
+        }
+        return result;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        SingleNumber obj = new SingleNumber();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(obj.singleNumber(nums)); // Output: 4
+    }
+}
